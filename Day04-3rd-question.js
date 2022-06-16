aad54e647ed80ae625d14a2ed59349244c25556d
shaikaziya <105929601+shaikaziya@users.noreply.github.com>
2022-06-16 19:30:49 +0530

    Added Day04-3rd-question.js file
    Added Day04-3rd-question.js file

---
+++ Day04-3rd-question.js
@@ -0,0 +1,11 @@
+
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response)
+    for(i=0;i<result.length;i++){
+        console.log("countryname---"+result[i].name+"   region---"+result[i].region+"   subregion---"+
+        result[i].subregion+"   population---"+result[i].population);
+    }
+}
